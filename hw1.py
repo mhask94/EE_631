2bf2db153b64a5cdacb23789465bf982ff5c61c1
Mat Haskell <mhaskell9@gmail.com>
2019-01-18 21:53:15 -0700

    Added key switching and difference mode

--- hw1.py
+++ hw1.py
@@ -8,15 +8,45 @@ for p in rm_python2:
 
 import cv2
 
+def runAbsDiff(video,old_frame):
+    ret,frame = video.read()
+    diff = cv2.absdiff(frame,old_frame)
+    return diff
+
 video = cv2.VideoCapture(-1)
+
+#prev_ret,prev_frame = video.read()
+mode = 0
 
 while (True):
     ret,frame = video.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Haskell',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    key = cv2.waitKey(1)
+    if key == ord('d'):
+        print('d pressed')
+        mode = 2
+    elif key == ord('g'):
+        print('g pressed')
+        mode = 1
+    elif key == ord('q'):
+        print('q pressed')
         break
+    elif key == ord('n'):
+        print('n pressed')
+        mode = 0
+
+    if mode == 1:
+        image = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    elif mode == 2:
+        image = runAbsDiff(video,frame)
+    else:
+        image = frame
+
+    cv2.imshow('Haskell',image)
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+    #    break
 
 video.release()
 cv2.destroyAllWindows()
+
 
