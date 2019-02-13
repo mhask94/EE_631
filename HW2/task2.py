14db16585ae8c1bf7142fe8de0ddde47baf2da10
Mat Haskell <mhaskell9@gmail.com>
2019-02-13 12:20:09 -0700

    Updated errors on HW assignment

--- HW2/task2.py
+++ HW2/task2.py
@@ -41,8 +41,8 @@ for filename in images:
 # run calibration
 ret,mat,dist,rvecs,tvecs = cv.calibrateCamera(obj_pts,img_pts,gray.shape[::-1],None,None)
 
-# camera spec sheet said pixel size is 4.7e-3 mm X 4.7e-3 mm
-mm_per_pix = 4.7e-3
+# camera spec sheet said pixel size is 7.4e-3 mm X 7.4e-3 mm
+mm_per_pix = 7.4e-3
 fSx = mat[0,0] # fist element of matrix is fSx in pixels
 focal_len = fSx * mm_per_pix
 
