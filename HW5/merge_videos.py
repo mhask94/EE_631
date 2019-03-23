c6518e88c8170a9c49beafbf9e7b521e2bb190df
Mat Haskell <mhaskell9@gmail.com>
2019-03-23 01:28:29 -0600

    Finished HW5

--- HW5/merge_videos.py
+++ HW5/merge_videos.py
@@ -1,12 +1,24 @@
 import cv2 as cv
 
+# task 1 vids
+## 1st video
+#vid_names = ['Task1_level0_skip0.avi','Task1_level0_skip20.avi']
+## 2nd video
+#vid_names = ['Task1_level3_skip0.avi','Task1_level3_skip20.avi']
+
 # task 2 vids
-vid_names = ['Task2_skip0.avi','Task2_skip20.avi']
+#vid_names = ['Task2_skip0.avi','Task2_skip20.avi']
+
+# task 3 vids
+vid_names = ['Task3_skip1.avi','Task3_skip20.avi']
 
 fps = 20
 size = (1920,1080)
 fourCC = cv.VideoWriter_fourcc(*'MPEG')
-writer = cv.VideoWriter('Task2.avi',fourCC,fps,size,isColor=1)
+#writer = cv.VideoWriter('Task1_level0.avi',fourCC,fps,size,isColor=1)
+#writer = cv.VideoWriter('Task1_level3.avi',fourCC,fps,size,isColor=1)
+#writer = cv.VideoWriter('Task2.avi',fourCC,fps,size,isColor=1)
+writer = cv.VideoWriter('Task3.avi',fourCC,fps,size,isColor=1)
 
 for name in vid_names:
     print('processing '+name)
